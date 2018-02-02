c7e61491d0cd4bb1755244a16672f00c2fa1479e
chenghsuanw <chenghsuan.w@gmail.com>
2018-02-02 22:41:27 +0800

    hw0

--- hw0/Q1.py
+++ hw0/Q1.py
@@ -1,24 +1,28 @@
 import sys
 
-f = open(sys.argv[1],'r')
-data = f.read()
-f.close()
-words = data.split()
-dealed = []
-count = []
-for word in words:
-	if word in dealed:
-		for index in range(len(dealed)):
-			if dealed[index] == word:
-				count[index] += 1
-				break
-	else:
-		dealed.append(word)
-		count.append(1)
-f = open('Q1.txt','w')
-for i in range(len(dealed)):
-	if i == len(dealed)-1:
-		f.write("%s %d %d" %(dealed[i], i, count[i]))
-	else:
-		f.write("%s %d %d\n" %(dealed[i], i, count[i]))
-f.close()
+def main():
+	with open(sys.argv[1], 'r') as f:
+		data = f.read()
+
+	words = data.split()
+	dealed = []
+	count = []
+	for word in words:
+		if word in dealed:
+			for index in range(len(dealed)):
+				if dealed[index] == word:
+					count[index] += 1
+					break
+		else:
+			dealed.append(word)
+			count.append(1)
+
+	with open('Q1.txt', 'w') as f:
+		for i in range(len(dealed)):
+			if i == len(dealed)-1:
+				f.write("%s %d %d" %(dealed[i], i, count[i]))
+			else:
+				f.write("%s %d %d\n" %(dealed[i], i, count[i]))
+
+if __name__ == '__main__':
+	main()
--- hw0/Q2.py
+++ hw0/Q2.py
@@ -1,15 +1,18 @@
 from PIL import Image
 import sys
 
-im = Image.open(sys.argv[1])
-width, height = im.size
-rgb = im.getpixel((1,1))
+def main():	
+	im = Image.open(sys.argv[1])
+	width, height = im.size
+	rgb = im.getpixel((1,1))
 
+	for y in range(height):
+		for x in range(width):
+			rgb = im.getpixel((x, y))
+			rgb = (rgb[0]//2, rgb[1]//2, rgb[2]//2)
+			im.putpixel((x, y), rgb)
 
-for y in range(height):
-	for x in range(width):
-		rgb = im.getpixel((x, y))
-		rgb = (rgb[0]//2, rgb[1]//2, rgb[2]//2)
-		im.putpixel((x, y), rgb)
+	im.save("Q2.png")
 
-im.save("Q2.png")
+if __name__ == '__main__':
+	main()
