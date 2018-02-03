0cb4a0fbc6b2f193605d9a0df216fbc69c6b11d9
chenghsuanw <chenghsuan.w@gmail.com>
2018-02-03 10:16:40 +0800

    change format

--- hw2/hw2_DNN.py
+++ hw2/hw2_DNN.py
@@ -7,40 +7,46 @@ from keras.optimizers import Adam
 from keras.utils import np_utils
 from keras.layers.normalization import BatchNormalization
 
-x = pd.read_csv("X_train.dms")
-y = pd.read_csv("Y_train.dms")
-x_test = pd.read_csv("X_test.dms")
-x_test = np.array(x_test)
-
-x = np.array(x)
-y = np.squeeze(y)
-y = np_utils.to_categorical(y)
-
-x[:,0] = (x[:,0]-np.mean(x[:,0]))/np.std(x[:,0])
-x[:,1] = (x[:,1]-np.mean(x[:,1]))/np.std(x[:,1])
-x[:,3] = (x[:,3]-np.mean(x[:,3]))/np.std(x[:,3])
-x[:,4] = (x[:,4]-np.mean(x[:,4]))/np.std(x[:,4])
-x[:,5] = (x[:,5]-np.mean(x[:,5]))/np.std(x[:,5])
-
-model = Sequential()
-model.add(Dense(input_dim=106, units=150, activation='relu'))
-model.add(BatchNormalization())
-for i in range(5):
-	model.add(Dense(units=150, activation='relu'))
+def build_model():
+	model = Sequential()
+	model.add(Dense(input_dim=106, units=150, activation='relu'))
 	model.add(BatchNormalization())
+	for i in range(5):
+		model.add(Dense(units=150, activation='relu'))
+		model.add(BatchNormalization())
 
-model.add(Dense(units=2, activation='softmax'))
+	model.add(Dense(units=2, activation='softmax'))
 
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-model.fit(x, y, validation_split=0, batch_size=1000, epochs=30)
+	model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-result = model.predict(x_test)
-result = result.astype(int)
+	return model
 
-f = open("nn.csv","w")
+def main():
+	x = pd.read_csv(sys.argv[1],encoding='big5')
+	y = pd.read_csv(sys.argv[2],encoding='big5')
+	x_test = pd.read_csv(sys.argv[3],encoding='big5')
+	x_test = np.array(x_test)
 
-f.write("id,label\n")
+	x = np.array(x)
+	y = np.squeeze(y)
+	y = np_utils.to_categorical(y)
 
-for i in range(result.shape[0]):
-	f.write(str(i+1)+","+str(result[i][1])+"\n")
-f.close()
+	x[:,0] = (x[:,0]-np.mean(x[:,0]))/np.std(x[:,0])
+	x[:,1] = (x[:,1]-np.mean(x[:,1]))/np.std(x[:,1])
+	x[:,3] = (x[:,3]-np.mean(x[:,3]))/np.std(x[:,3])
+	x[:,4] = (x[:,4]-np.mean(x[:,4]))/np.std(x[:,4])
+	x[:,5] = (x[:,5]-np.mean(x[:,5]))/np.std(x[:,5])
+
+	model = build_model()
+	model.fit(x, y, validation_split=0, batch_size=1000, epochs=30)
+
+	result = model.predict(x_test)
+	result = result.astype(int)
+
+	with open(sys.argv[4], 'w') as f:
+		f.write("id,label\n")
+		for i in range(result.shape[0]):
+			f.write(str(i+1)+","+str(result[i][1])+"\n")
+	
+if __name__ == '__main__':
+	main()
--- hw2/hw2_logistic.py
+++ hw2/hw2_logistic.py
@@ -2,58 +2,59 @@ import numpy as np
 import pandas as pd
 import sys
 
-x = pd.read_csv(sys.argv[1])
-y = pd.read_csv(sys.argv[2])
+def main():
+	x = pd.read_csv(sys.argv[1])
+	y = pd.read_csv(sys.argv[2])
 
-x = np.array(x)
-y = np.squeeze(y)
+	x = np.array(x)
+	y = np.squeeze(y)
 
-#add bias
-x = np.concatenate((np.ones((x.shape[0],1)),x),axis=1)
+	#add bias
+	x = np.concatenate((np.ones((x.shape[0],1)),x),axis=1)
 
-#normalize
-x[:,1] = (x[:,1]-np.mean(x[:,1]))/np.std(x[:,1])
-x[:,2] = (x[:,2]-np.mean(x[:,2]))/np.std(x[:,2])
-x[:,4] = (x[:,4]-np.mean(x[:,4]))/np.std(x[:,4])
-x[:,6] = (x[:,6]-np.mean(x[:,6]))/np.std(x[:,6])
+	#normalize
+	x[:,1] = (x[:,1]-np.mean(x[:,1]))/np.std(x[:,1])
+	x[:,2] = (x[:,2]-np.mean(x[:,2]))/np.std(x[:,2])
+	x[:,4] = (x[:,4]-np.mean(x[:,4]))/np.std(x[:,4])
+	x[:,6] = (x[:,6]-np.mean(x[:,6]))/np.std(x[:,6])
 
-w = np.zeros(len(x[0]))
-l_rate = 1.75e-2
-reg = 1
-repeat = 10000
+	w = np.zeros(len(x[0]))
+	l_rate = 1.75e-2
+	reg = 1
+	repeat = 10000
 
-ada_sum = np.zeros(len(x[0]))
-#training
-for i in range(repeat):
-	pred = (1/(1+np.exp(-np.dot(x,w))))
-	loss = pred - y
-	gra = np.dot(loss, x) + 2*reg*w
-	ada_sum += gra**2
-	ada = np.sqrt(ada_sum)
-	w -= l_rate * gra / ada
+	ada_sum = np.zeros(len(x[0]))
+	#training
+	for i in range(repeat):
+		pred = (1/(1+np.exp(-np.dot(x,w))))
+		loss = pred - y
+		gra = np.dot(loss, x) + 2*reg*w
+		ada_sum += gra**2
+		ada = np.sqrt(ada_sum)
+		w -= l_rate * gra / ada
 
-	result = np.rint(pred)
-	err = np.sum(np.absolute(result - y))
-	print("iteration",i,"\t",1 - err/len(y),sep='')
+		result = np.rint(pred)
+		err = np.sum(np.absolute(result - y))
+		print("iteration",i,"\t",1 - err/len(y),sep='')
 
-#testing
-x_test = pd.read_csv(sys.argv[3])
-x_test = np.array(x_test)
-x_test = np.concatenate((np.ones((x_test.shape[0],1)),x_test),axis=1)
+	#testing
+	x_test = pd.read_csv(sys.argv[3])
+	x_test = np.array(x_test)
+	x_test = np.concatenate((np.ones((x_test.shape[0],1)),x_test),axis=1)
 
-x_test[:,1] = (x_test[:,1]-np.mean(x_test[:,1]))/np.std(x_test[:,1])
-x_test[:,2] = (x_test[:,2]-np.mean(x_test[:,2]))/np.std(x_test[:,2])
-x_test[:,4] = (x_test[:,4]-np.mean(x_test[:,4]))/np.std(x_test[:,4])
-x_test[:,6] = (x_test[:,6]-np.mean(x_test[:,6]))/np.std(x_test[:,6])
+	x_test[:,1] = (x_test[:,1]-np.mean(x_test[:,1]))/np.std(x_test[:,1])
+	x_test[:,2] = (x_test[:,2]-np.mean(x_test[:,2]))/np.std(x_test[:,2])
+	x_test[:,4] = (x_test[:,4]-np.mean(x_test[:,4]))/np.std(x_test[:,4])
+	x_test[:,6] = (x_test[:,6]-np.mean(x_test[:,6]))/np.std(x_test[:,6])
 
-pred_test = (1/(1+np.exp(-np.dot(x_test,w))))
-result_test = np.rint(pred_test)
-result_test = result_test.astype(int)
+	pred_test = (1/(1+np.exp(-np.dot(x_test,w))))
+	result_test = np.rint(pred_test)
+	result_test = result_test.astype(int)
 
-f = open(sys.argv[4],"w")
+	with open(sys.argv[4], 'w') as f:
+		f.write("id,label\n")
+		for i in range(len(result_test)):
+			f.write(str(i+1)+","+str(result_test[i])+"\n")
 
-f.write("id,label\n")
-
-for i in range(len(result_test)):
-	f.write(str(i+1)+","+str(result_test[i])+"\n")
-f.close()
+if __name__ == '__main__':
+	main()
